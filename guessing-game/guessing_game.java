51f1306efc5ca2350ec7a3d47e0e546dc8df0c9d
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2023-10-27 22:34:49 +0200

    Create guessing_game.java

---
+++ guessing-game/guessing_game.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessing_game {
+    public static void main(String[] args) {
+
+        boolean correctGuess = false;
+
+        System.out.println("Enter a number from 1-10: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        Random rnd = new Random();
+
+        int num = rnd.nextInt(10) + 1;
+
+        while (correctGuess == false) {
+            int guess = sc.nextInt();
+        
+            if (guess == num) {
+                System.out.println("You guessed correct! The number is " + num);
+                break;
+            }
+
+            else {
+                System.out.println("Incorrect guess! Try again.");
+            }
+        }
+
+    }
+
+}
