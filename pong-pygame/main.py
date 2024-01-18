984823eb975cb63fe9b577396f451ad25232bd63
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-01-18 14:54:40 +0200

    fix bug in starting game

--- pong-pygame/main.py
+++ pong-pygame/main.py
@@ -19,25 +19,45 @@ BALL_VEL = 7
 PLAYER_1_BAT_VEL = 10
 PLAYER_2_BAT_VEL = 10
 
-text_font = pygame.font.SysFont(None, 36)
+text_font = pygame.font.SysFont("arial", 36)
 text_color = (255, 255, 255)
 scores = [0,0]
 player_score_pos = (WIDTH // 3, HEIGHT // 3)
 enemy_score_pos = (WIDTH - WIDTH // 3, HEIGHT // 3)
 message_pos = (WIDTH // 2, HEIGHT // 2)
 
-show_welcome_screen = True
-show_game_screen = False
-show_end_screen = False
+# show_welcome_screen = True
+# show_game_screen = False
+# show_end_screen = False
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     run = True
 
-    player_object = player_bat (pos_x=10, pos_y=HEIGHT//2, width=10, height=150, vel=PLAYER_1_BAT_VEL)
-    enemy_object = enemy_bat (pos_x=WIDTH-20, pos_y=HEIGHT//2, width=10, height=150, vel=PLAYER_2_BAT_VEL)    
-    ball_object = ball(pos_x=WIDTH/2, pos_y=HEIGHT/2, width = 20, vel=BALL_VEL)
+    show_welcome_screen = True
+    show_game_screen = False
+    show_end_screen = False
+
+    player_object = player_bat (
+        pos_x=10, 
+        pos_y=HEIGHT//2, 
+        width=10, 
+        height=150, 
+        vel=PLAYER_1_BAT_VEL)
+    
+    enemy_object = enemy_bat (
+        pos_x=WIDTH-20, 
+        pos_y=HEIGHT//2, 
+        width=10, 
+        height=150, 
+        vel=PLAYER_2_BAT_VEL)    
+    
+    ball_object = ball(
+        pos_x=WIDTH/2, 
+        pos_y=HEIGHT/2, 
+        width = 20, 
+        vel=BALL_VEL)
 
     while run:
         for event in pygame.event.get():
@@ -52,25 +72,64 @@ def main():
         screen.fill("black")
 
         if show_welcome_screen:
-            draw.draw_welcome_message(screen,text_font,text_color,scores,WIDTH,HEIGHT,player_score_pos,enemy_score_pos,message_pos,ball_object,enemy_object,player_object)
+            draw.draw_welcome_message(
+                screen,text_font,
+                text_color,
+                scores,
+                WIDTH,HEIGHT,
+                player_score_pos,
+                enemy_score_pos,
+                message_pos,
+                ball_object,
+                enemy_object,
+                player_object)
+            
+            while show_game_screen == False:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        show_game_screen = True
+                        show_welcome_screen = False
 
+        if show_game_screen:
+            # game.restart_game(ball_object,
+            #                   BALL_VEL,
+            #                   enemy_object,
+            #                   player_object,
+            #                   WIDTH,
+            #                   HEIGHT,
+            #                   scores)
+            
+            draw.draw_game_screen(screen,
+                                  text_font,
+                                  text_color,
+                                  scores,
+                                  WIDTH,
+                                  HEIGHT,
+                                  player_score_pos,
+                                  enemy_score_pos,
+                                  message_pos,
+                                  ball_object,
+                                  enemy_object,
+                                  player_object)
+
+
+        if show_end_screen:
+            
+            draw.draw_end_screen(screen,
+                                 text_font,
+                                 text_color,
+                                 scores,
+                                 WIDTH,
+                                 HEIGHT,
+                                 player_score_pos,
+                                 enemy_score_pos,
+                                 message_pos)
+            
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     show_game_screen = True
                     show_welcome_screen = False
 
-        if show_game_screen:
-
-            game.restart_game()
-            
-            draw.draw_game_screen(screen,text_font,text_color,scores,WIDTH,HEIGHT,player_score_pos,enemy_score_pos,message_pos,ball_object,enemy_object,player_object)
-
-
-        if show_end_screen:
-            
-            draw.draw_end_screen(screen,text_font,text_color,scores,WIDTH,HEIGHT,player_score_pos,enemy_score_pos,message_pos)
-
-        draw.draw_game_screen(screen, ball_object,enemy_object,player_object,text_font,text_color,scores,WIDTH,HEIGHT,player_score_pos,enemy_score_pos)
         # draw.draw_objects(ball_object,enemy_object,player_object,screen)
 
         # draw.draw_text(screen,text_font,text_color,scores, WIDTH, HEIGHT, player_score_pos, enemy_score_pos)
