4163f53a9a09a9dd72175642328a1c0e3dc2edf7
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-02-09 18:53:17 +0200

    modify ViewController to use image processor

--- Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ViewController.swift
+++ Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ViewController.swift
@@ -1,96 +1,55 @@
-//
-//  ViewController.swift
-//  Filterer
-//
-//
-
+// ViewController.swift
 import UIKit
+import ImageProcessor
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    var originalImage: UIImage?
     var filteredImage: UIImage?
+    var filterIntensity: Float = 1.0
     
     @IBOutlet var imageView: UIImageView!
-    
     @IBOutlet var secondaryMenu: UIView!
-    
     @IBOutlet var bottomMenu: UIView!
-    
     @IBOutlet var imageToggle: UIButton!
-    
     @IBOutlet var filterButton: UIButton!
+    @IBOutlet var editButton: UIButton!
     
+    let processor = ImageProcessor()
     
-    @IBAction func onImageToggle(sender: UIButton) {
-        imageView.image = filteredImage
-        
-        if imageToggle.selected {
-            let image = UIImage(named: "scenery")!
-            imageView.image = image
-            imageToggle.selected = false
-        } else {
-            imageView.image = filteredImage
-            imageToggle.selected = true
-        }
-        
-
-        
-    }
-
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imageToggle.setTitle("Show Before Image", forState: .Selected)
         
         let image = UIImage(named: "scenery")!
+        originalImage = image
+        
         var rgbaImage = RGBAImage(image: image)!
-        let pixelCount = rgbaImage.width * rgbaImage.height
-        let avgRed = 107
-        let avgGreen = 109
-        let avgBlue = 110
-        let sum = avgRed + avgGreen + avgBlue
-        for y in 0..<rgbaImage.height {
-            for x in 0..<rgbaImage.width {
-                
-                let index = y * rgbaImage.width + x
-                
-                var pixel = rgbaImage.pixels [index]
-                
-                let redDelta = Int (pixel.red) - avgRed
-                let greenDelta = Int(pixel.green) - avgGreen
-                let blueDelta = Int(pixel.green) - avgBlue
-                
-                var modifier = 1 + 4 * (Double(y) / Double(rgbaImage.height))
-                
-                if (Int (pixel.red) < avgRed) {
-                    modifier = 1
-                }
-                
-                pixel.red = UInt8(max(min(255, Int(round(Double(avgRed) + modifier * Double(redDelta)))), 0))
-                
-                rgbaImage.pixels[index] = pixel
-            }
-        }
         
-        filteredImage = rgbaImage.toUIImage()
+        applyFilter(&rgbaImage) // Pass rgbaImage by reference
         
-        print("This code has executed")
-        
-
+        imageView.image = filteredImage
         
         secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
     }
-
-    // MARK: Share
+    
+    @IBAction func onImageToggle(sender: UIButton) {
+        if sender.selected {
+            imageView.image = filteredImage
+        } else {
+            imageView.image = originalImage
+        }
+        
+        sender.selected = !sender.selected
+    }
+    
     @IBAction func onShare(sender: AnyObject) {
         let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", imageView.image!], applicationActivities: nil)
         presentViewController(activityController, animated: true, completion: nil)
     }
     
-    // MARK: New Photo
     @IBAction func onNewPhoto(sender: AnyObject) {
         let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
         
@@ -104,7 +63,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         
-        self.presentViewController(actionSheet, animated: true, completion: nil)
+        presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    @IBAction func onFilter(sender: UIButton) {
+        if sender.selected {
+            hideSecondaryMenu()
+            sender.selected = false
+        } else {
+            showSecondaryMenu()
+            sender.selected = true
+        }
+    }
+    
+    @IBAction func onEdit(sender: AnyObject) {
+        guard let _ = filterButton.currentTitle else { return }
+        hideSecondaryMenu()
+        
+        let slider = UISlider(frame: CGRect(x: 20, y: self.view.frame.height - 100, width: self.view.frame.width - 40, height: 40))
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.value = filterIntensity
+        slider.addTarget(self, action: #selector(sliderValueChanged(_:)), forControlEvents: .ValueChanged)
+        self.view.addSubview(slider)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            originalImage = image
+            var rgbaImage = RGBAImage(image: image)!
+            applyFilter(&rgbaImage) // Pass rgbaImage by reference
+            imageView.image = filteredImage
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func applyFilter(inout rgbaImage: RGBAImage) {
+        // Call the ImageProcessor to apply the filter
+        processor.applyFilter(to: &rgbaImage, filterStrength: 10, filterColor: "Red")
+        filteredImage = rgbaImage.toUIImage()
+    }
+    
+    func sliderValueChanged(sender: UISlider) {
+        filterIntensity = sender.value
+        var rgbaImage = RGBAImage(image: originalImage!)! // Apply filter on the original image
+        applyFilter(&rgbaImage) // Pass rgbaImage by reference
+        imageView.image = filteredImage
     }
     
     func showCamera() {
@@ -121,29 +129,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         cameraPicker.sourceType = .PhotoLibrary
         
         presentViewController(cameraPicker, animated: true, completion: nil)
-    }
-    
-    // MARK: UIImagePickerControllerDelegate
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        dismissViewControllerAnimated(true, completion: nil)
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            imageView.image = image
-        }
-    }
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    // MARK: Filter Menu
-    @IBAction func onFilter(sender: UIButton) {
-        if (sender.selected) {
-            hideSecondaryMenu()
-            sender.selected = false
-        } else {
-            showSecondaryMenu()
-            sender.selected = true
-        }
     }
     
     func showSecondaryMenu() {
@@ -164,16 +149,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.secondaryMenu.alpha = 1.0
         }
     }
-
+    
     func hideSecondaryMenu() {
         UIView.animateWithDuration(0.4, animations: {
             self.secondaryMenu.alpha = 0
-            }) { completed in
-                if completed == true {
-                    self.secondaryMenu.removeFromSuperview()
-                }
+        }) { completed in
+            if completed == true {
+                self.secondaryMenu.removeFromSuperview()
+            }
         }
     }
-
 }
-
