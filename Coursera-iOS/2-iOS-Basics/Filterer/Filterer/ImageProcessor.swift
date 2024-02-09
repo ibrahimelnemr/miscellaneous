1f06da95ff057d7daa5502e1062f581c3a54f847
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-02-09 19:41:05 +0200

    allow for using imageprocessor in viewcontroller

--- Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ImageProcessor.swift
+++ Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ImageProcessor.swift
@@ -1,95 +1,95 @@
 //ImageProcessor.swift
-import Filter
-
-public class ImageProcessor {
-    init() {
-        
-    }
-    
-    
-    
-    static func applyFilter(inout to myRGBA: RGBAImage, filterStrength: Int, filterColor: String) {
-        var totalRed = 0
-        var totalGreen = 0
-        var totalBlue = 0
-
-        for y in 0..<myRGBA.height {
-            for x in 0..<myRGBA.width {
-                let index = y * myRGBA.width + x
-                var pixel = myRGBA.pixels[index]
-                totalRed += Int(pixel.red)
-                totalGreen += Int(pixel.green)
-                totalBlue += Int(pixel.blue)
-            }
-        }
-        
-        
-
-        let count = myRGBA.width * myRGBA.height
-        let avgRed = totalRed/count
-        let avgGreen = totalGreen/count
-        let avgBlue = totalBlue/count
-
-        
-        let acceptedColors = [
-            "Green",
-            "Red",
-            "Blue"
-        ]
-        
-        let acceptedStrengths = [1,2,3,4,5,6,7,8,9,10]
-        
-        if !acceptedColors.contains(filterColor) || !acceptedStrengths.contains(filterStrength) {
-            print("Invalid filter or strength value")
-            return
-        }
-
-        
-        if filterColor == "Red" {
-            
-            for y in 0..<myRGBA.height {
-                for x in 0..<myRGBA.width {
-                    let index = y * myRGBA.width + x
-                    var pixel = myRGBA.pixels[index]
-                    let redDiff = Int(pixel.red) - avgRed
-                    if (redDiff > 0) {
-                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
-                        myRGBA.pixels[index] = pixel
-                    }
-                }
-            }
-
-        }
-        else if filterColor == "Blue" {
-            for y in 0..<myRGBA.height {
-                for x in 0..<myRGBA.width {
-                    let index = y * myRGBA.width + x
-                    var pixel = myRGBA.pixels[index]
-                    let redDiff = Int(pixel.red) - avgRed
-                    if (redDiff > 0) {
-                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
-                        myRGBA.pixels[index] = pixel
-                    }
-                }
-            }
-
-            
-        }
-        else if filterColor == "Green" {
-            
-            for y in 0..<myRGBA.height {
-                for x in 0..<myRGBA.width {
-                    let index = y * myRGBA.width + x
-                    var pixel = myRGBA.pixels[index]
-                    let redDiff = Int(pixel.red) - avgRed
-                    if (redDiff > 0) {
-                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
-                        myRGBA.pixels[index] = pixel
-                    }
-                }
-            }
-            
-        }
-     
-    }
-}
+//import Filter
+//
+//public class ImageProcessor {
+//    init() {
+//        
+//    }
+//    
+//    
+//    
+//    static func applyFilter(inout to myRGBA: RGBAImage, filterStrength: Int, filterColor: String) {
+//        var totalRed = 0
+//        var totalGreen = 0
+//        var totalBlue = 0
+//        
+//        for y in 0..<myRGBA.height {
+//            for x in 0..<myRGBA.width {
+//                let index = y * myRGBA.width + x
+//                var pixel = myRGBA.pixels[index]
+//                totalRed += Int(pixel.red)
+//                totalGreen += Int(pixel.green)
+//                totalBlue += Int(pixel.blue)
+//            }
+//        }
+//        
+//        
+//        
+//        let count = myRGBA.width * myRGBA.height
+//        let avgRed = totalRed/count
+//        let avgGreen = totalGreen/count
+//        let avgBlue = totalBlue/count
+//        
+//        
+//        let acceptedColors = [
+//            "Green",
+//            "Red",
+//            "Blue"
+//        ]
+//        
+//        let acceptedStrengths = [1,2,3,4,5,6,7,8,9,10]
+//        
+//        if !acceptedColors.contains(filterColor) || !acceptedStrengths.contains(filterStrength) {
+//            print("Invalid filter or strength value")
+//            return
+//        }
+//        
+//        
+//        if filterColor == "Red" {
+//            
+//            for y in 0..<myRGBA.height {
+//                for x in 0..<myRGBA.width {
+//                    let index = y * myRGBA.width + x
+//                    var pixel = myRGBA.pixels[index]
+//                    let redDiff = Int(pixel.red) - avgRed
+//                    if (redDiff > 0) {
+//                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
+//                        myRGBA.pixels[index] = pixel
+//                    }
+//                }
+//            }
+//            
+//        }
+//        else if filterColor == "Blue" {
+//            for y in 0..<myRGBA.height {
+//                for x in 0..<myRGBA.width {
+//                    let index = y * myRGBA.width + x
+//                    var pixel = myRGBA.pixels[index]
+//                    let redDiff = Int(pixel.red) - avgRed
+//                    if (redDiff > 0) {
+//                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
+//                        myRGBA.pixels[index] = pixel
+//                    }
+//                }
+//            }
+//            
+//            
+//        }
+//        else if filterColor == "Green" {
+//            
+//            for y in 0..<myRGBA.height {
+//                for x in 0..<myRGBA.width {
+//                    let index = y * myRGBA.width + x
+//                    var pixel = myRGBA.pixels[index]
+//                    let redDiff = Int(pixel.red) - avgRed
+//                    if (redDiff > 0) {
+//                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
+//                        myRGBA.pixels[index] = pixel
+//                    }
+//                }
+//            }
+//            
+//        }
+//        
+//    }
+//}
--- Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ViewController.swift
+++ Coursera-iOS/2-iOS-Basics/Filterer/Filterer/ViewController.swift
@@ -1,6 +1,6 @@
+
 // ViewController.swift
 import UIKit
-import ImageProcessor
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -156,6 +156,85 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }) { completed in
             if completed == true {
                 self.secondaryMenu.removeFromSuperview()
+            }
+        }
+    }
+}
+
+
+public class ImageProcessor {
+    init() {
+        
+    }
+    
+    func applyFilter(inout to myRGBA: RGBAImage, filterStrength: Int, filterColor: String) {
+        var totalRed = 0
+        var totalGreen = 0
+        var totalBlue = 0
+        
+        for y in 0..<myRGBA.height {
+            for x in 0..<myRGBA.width {
+                let index = y * myRGBA.width + x
+                var pixel = myRGBA.pixels[index]
+                totalRed += Int(pixel.red)
+                totalGreen += Int(pixel.green)
+                totalBlue += Int(pixel.blue)
+            }
+        }
+        
+        let count = myRGBA.width * myRGBA.height
+        let avgRed = totalRed / count
+        let avgGreen = totalGreen / count
+        let avgBlue = totalBlue / count
+        
+        let acceptedColors = [
+            "Green",
+            "Red",
+            "Blue"
+        ]
+        
+        let acceptedStrengths = [1,2,3,4,5,6,7,8,9,10]
+        
+        if !acceptedColors.contains(filterColor) || !acceptedStrengths.contains(filterStrength) {
+            print("Invalid filter or strength value")
+            return
+        }
+        
+        if filterColor == "Red" {
+            for y in 0..<myRGBA.height {
+                for x in 0..<myRGBA.width {
+                    let index = y * myRGBA.width + x
+                    var pixel = myRGBA.pixels[index]
+                    let redDiff = Int(pixel.red) - avgRed
+                    if redDiff > 0 {
+                        pixel.red = UInt8(max(0, min(255, avgRed + redDiff * filterStrength)))
+                        myRGBA.pixels[index] = pixel
+                    }
+                }
+            }
+        } else if filterColor == "Blue" {
+            for y in 0..<myRGBA.height {
+                for x in 0..<myRGBA.width {
+                    let index = y * myRGBA.width + x
+                    var pixel = myRGBA.pixels[index]
+                    let blueDiff = Int(pixel.blue) - avgBlue
+                    if blueDiff > 0 {
+                        pixel.blue = UInt8(max(0, min(255, avgBlue + blueDiff * filterStrength)))
+                        myRGBA.pixels[index] = pixel
+                    }
+                }
+            }
+        } else if filterColor == "Green" {
+            for y in 0..<myRGBA.height {
+                for x in 0..<myRGBA.width {
+                    let index = y * myRGBA.width + x
+                    var pixel = myRGBA.pixels[index]
+                    let greenDiff = Int(pixel.green) - avgGreen
+                    if greenDiff > 0 {
+                        pixel.green = UInt8(max(0, min(255, avgGreen + greenDiff * filterStrength)))
+                        myRGBA.pixels[index] = pixel
+                    }
+                }
             }
         }
     }
