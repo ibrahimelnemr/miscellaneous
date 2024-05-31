08efbff768a540fce1d70e7bc082e1d7d2d67870
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-05-31 20:44:30 +0300

    remove java raylib pong

--- game-dev-miscellaneous/raylib-misc/java-raylib-pong/app/src/main/java/org/example/Interfaces/IBasicObject.java
+++
@@ -1,9 +0,0 @@
-package org.example.Interfaces;
-
-import com.raylib.Jaylib.Vector2;
-
-public interface IBasicObject {
-    public void draw();
-    public Vector2 getPosition();
-    public void setPosition(Vector2 position );
-}
--- game-dev-miscellaneous/raylib-misc/java-raylib-pong/app/src/main/java/org/example/PlayerBat.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import com.raylib.Jaylib.Vector2;
-
-public class PlayerBat extends Bat {
-        public PlayerBat(Vector2 position, Vector2 size, int velocity) {
-        super(position, size, velocity);
-    }
-}
